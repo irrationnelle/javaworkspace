a4dff314cf891337a375ea9e153c9a5a42f2474b
irrationnelle <drakkarverenis@gmail.com>
2016-08-23 11:27:15 +0900

    01 first commit

---
+++ Day02/src/Test02.java
@@ -0,0 +1,9 @@
+
+public class Test02 {
+	public static void main(String[] args) {
+		System.out.println("첫번째 텍스트");
+		System.out.println("두번째 텍스트");
+		System.out.println("세번째 텍스트");
+		System.out.println("네번째 텍스트");		
+	}
+}
---
+++ Day02/src/Test03.java
@@ -0,0 +1,17 @@
+
+public class Test03 {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		int sum;
+		
+		x = 100;
+		y = 200;
+		sum = x+y;
+		
+		System.out.println(sum);
+		return;
+	}
+
+}
