693bb0e8cca1de849d0f6d80660af2357d83c6cb
irrationnelle <drakkarverenis@gmail.com>
2016-08-23 17:31:10 +0900

    06 modify semantic error

--- Day02/src/Test10.java
+++ Day02/src/Test10.java
@@ -10,7 +10,8 @@ public class Test10 {
 		System.out.print("input radius: ");
 		rad = input.nextDouble();
 		
-		volume = 4/3*rad*rad*rad;
+		volume = 4.0/3.0*rad*rad*rad;
+		// 4/3일 경우, 정수값인 1이 나오기 때문에 확실하게 실수 표기해준다.
 		
 		System.out.println("Sphere volume: "+volume);
 	}
