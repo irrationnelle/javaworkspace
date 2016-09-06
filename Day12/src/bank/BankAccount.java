3565e2717ad0ba0e666eda385082666200ce3a16
irrationnelle <drakkarverenis@gmail.com>
2016-09-06 16:22:36 +0900

    02 completion to implement

--- Day12/src/bank/BankAccount.java
+++ Day12/src/bank/BankAccount.java
@@ -5,35 +5,54 @@ public class BankAccount {
 	private String password;
 	private int balance;
 	private int account;
-	public static int accountNum = 11111;
+	public static int accountNum = 11110;
 	
-	
-	public void signUp() {
-		System.out.print("이름을 입력하세요: ");
-		System.out.print("비밀번호를 입력하세요: ");
+	public BankAccount(String name, String password, int initAmount) {
+		this.name = name;
+		this.setPassword(password);
+		this.balance = initAmount;
+		this.setAccount(accountNum++);
 		System.out.println("회원가입이 완료되었습니다.");
-		this.account = accountNum++;
+		System.out.println("**********계좌 정보**********");
+		this.checkAccout();
 	}
 	
-	public void deposit() {
-		System.out.print("계좌번호를 입력하세요: ");
-		System.out.print("입금 금액: ");
+	public void deposit(int depositAmount) {
+		this.balance += depositAmount;
 		System.out.println("입금이 완료되었습니다.");
 	}
 	
-	public void withdraw() {
-		System.out.print("계좌번호를 입력하세요: ");
-		System.out.print("비밀번호를 입력하세요: ");
+	public void withdraw(int withdrawAmount) {
 		System.out.print("출금 금액: ");
+		if(balance >= withdrawAmount) {
+			this.balance -= withdrawAmount;
+		} else {
+			System.out.println("잔액이 부족합니다.");
+			return;
+		}
 		System.out.println("출금이 완료되었습니다: ");
 	}
 	
 	public void checkAccout() {
-		System.out.print("계좌번호를 입력하세요: ");
-		System.out.print("비밀번호를 입력하세요: ");
 		System.out.println("계좌주: "+this.name);
-		System.out.println("계좌번호: "+this.accountNum);
+		System.out.println("계좌번호: "+this.account);
 		System.out.println("현재 잔액: "+this.balance);
 		
+	}
+
+	public int getAccount() {
+		return account;
+	}
+
+	public void setAccount(int account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
--- Day12/src/bank/BankAccountTest.java
+++ Day12/src/bank/BankAccountTest.java
@@ -4,37 +4,98 @@ import java.util.Scanner;
 
 public class BankAccountTest {
 	public static void main(String[] args) {
-		BankAccount[] accounts = new BankAccount[100]; 
+		BankAccount[] accounts = new BankAccount[100];
 
 		Scanner scan = new Scanner(System.in);
 		
-		
 		boolean isContinue = true;
+		int accountNum=0;
 		
 		while(isContinue) {
 			System.out.println("-- 1: 가입 2:입금 3:출금 4:계좌조회 -1:종료 --");
 			System.out.print("-- select >");
 			int input = scan.nextInt();
+			int i=0;
+			boolean isCorrect = false;
+			int account;
+			String checkPassword;
 			switch(input) {
-			case 1:
-				// 객체 생성
-				System.out.println("가입한다");
+			case 1: // 객체(계좌) 생성
+				System.out.print("이름을 입력하세요: ");
+				String name = scan.next();
+				System.out.print("비밀번호를 입력하세요: ");
+				String password = scan.next();
+				System.out.print("초기금액을 입력하세요: ");
+				int initAmount = scan.nextInt();
+				accounts[accountNum] = new BankAccount(name, password, initAmount);
+				accountNum++;
 				break;
-			case 2:
-				// setters, getters 사용
-				System.out.println("입금한다");
-				break;
-			case 3:
-				// setters, getters 사용
-				System.out.println("출금한다");
-				break;
-			case 4:
-				System.out.println("조회한다");
-				break;
-			case -1:
+			case 2: // 입금
+				System.out.print("계좌번호를 입력하세요: ");
+				account = scan.nextInt();
+				System.out.print("비밀번호를 입력하세요: ");
+				checkPassword = scan.next();
+				while(accounts[i] != null) {
+					if((account == accounts[i].getAccount()) && (checkPassword.equals(accounts[i].getPassword()))) {
+						System.out.print("입금 금액: ");
+						int depositAmount = scan.nextInt();
+						accounts[i].deposit(depositAmount);
+						isCorrect = true;
+						break;
+					}
+					i++;
+				}
+				if(isCorrect) {
+					break;
+				} else {
+					System.out.println("잘못된 계좌번호 혹은 비밀번호입니다.");
+					break;
+				}
+			case 3: // 출금
+				System.out.print("계좌번호를 입력하세요: ");
+				account = scan.nextInt();
+				System.out.print("비밀번호를 입력하세요: ");
+				checkPassword = scan.next();
+				while(accounts[i] != null) {
+					if((account == accounts[i].getAccount()) && (checkPassword.equals(accounts[i].getPassword()))) {
+						System.out.print("출금 금액: ");
+						int withdrawAmount = scan.nextInt();
+						accounts[i].withdraw(withdrawAmount);
+						isCorrect = true;
+						break;
+					}
+					i++;
+				}
+				if(isCorrect) {
+					break;
+				} else {
+					System.out.println("잘못된 계좌번호 혹은 비밀번호입니다.");
+					break;
+				}
+			case 4: // 조회
+				System.out.print("계좌번호를 입력하세요: ");
+				account = scan.nextInt();
+				System.out.print("비밀번호를 입력하세요: ");
+				checkPassword = scan.next();
+				while(accounts[i] != null) {
+					if((account == accounts[i].getAccount()) && (checkPassword.equals(accounts[i].getPassword()))) {
+						accounts[i].checkAccout();
+						isCorrect = true;
+						break;
+					}
+					i++;
+				}
+				if(isCorrect) {
+					break;
+				} else {
+					System.out.println("잘못된 계좌번호 혹은 비밀번호입니다.");
+					break;
+				}
+			case -1: // 프로그램 종료
 				isContinue = false;
+				System.out.println("프로그램을 종료합니다.");
 				break;
-			default:
+			default: // 오입력
 				System.out.println("알맞은 번호를 다시 입력하세요.");
 			}
 		}
